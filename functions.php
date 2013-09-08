84df43f1a788f612f57a1baa9024c7e3784dbda4
Janne Saarela <janne.juhani@gmail.com>
2013-09-08 16:14:40 +0300

    Update favicon

--- functions.php
+++ functions.php
@@ -11,6 +11,6 @@ if ( !defined('ABSPATH')) exit;
  */
 // add a favicon
 function blog_favicon() {
-    echo '<link rel="shortcut icon" type="image/png" href="' . get_stylesheet_directory_uri() . '/images/favicon.png" />';
+    echo '<link rel="shortcut icon" type="image/png" href="' . get_stylesheet_directory_uri() . '/images/favicon.png?=1" />';
 }
 add_action('wp_head', 'blog_favicon');
