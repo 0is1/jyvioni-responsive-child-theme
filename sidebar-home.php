7a0fb62e60115e32dfe05d09531a23544f0cf6bc
Janne Saarela <janne.juhani@gmail.com>
2013-10-15 13:44:44 +0300

    Add homepage-posts to siderbar-home for now

--- sidebar-home.php
+++ sidebar-home.php
@@ -9,6 +9,7 @@ if ( !defined('ABSPATH')) exit;
  *
  * @file           sidebar-home.php
  * @package        Responsive
+ * @subpackage     Jyvioni-2013
  * @author         Emil Uzelac
  * @copyright      2003 - 2013 ThemeID
  * @license        license.txt
@@ -18,19 +19,23 @@ if ( !defined('ABSPATH')) exit;
  * @since          available since Release 1.0
  */
 ?>
-<?php  // We want to use only two home widgets ?>
+<?php  // We want to use only two home widgets in Jyvioni-2013 ?>
 
     <div id="widgets" class="home-widgets">
-        <div class="grid col-620">
-        <?php responsive_widgets(); // above widgets hook ?>
+      <div class="grid col-620">
+        <div class="widget-wrapper">
+        <?php
+          /* Add homepage-posts template */
+          get_template_part('homepage', 'posts');
+        ?>
+       </div><!-- end of .widget-wrapper -->
 
-            <?php if (!dynamic_sidebar('home-widget-1')) : ?>
-            <div class="widget-wrapper">
+      <?php responsive_widgets(); // above widgets hook ?>
 
-                <div class="widget-title-home"><h3><?php _e('Home Widget 1', 'responsive'); ?></h3></div>
-                <div class="textwidget"><?php _e('This is your first home widget box. To edit please go to Appearance > Widgets and choose 6th widget from the top in area 6 called Home Widget 1. Title is also manageable from widgets as well.','responsive'); ?></div>
-
-			</div><!-- end of .widget-wrapper -->
+      <?php if (!dynamic_sidebar('home-widget-1')) : ?>
+        <div class="widget-title-home"><h3><?php _e('Home Widget 1', 'responsive'); ?></h3></div>
+            <div class="textwidget"><?php _e('This is your first home widget box. To edit please go to Appearance > Widgets and choose 6th widget from the top in area 6 called Home Widget 1. Title is also manageable from widgets as well.','responsive'); ?>
+            </div>
 			<?php endif; //end of home-widget-1 ?>
 
         <?php responsive_widgets_end(); // responsive after widgets hook ?>
@@ -40,11 +45,9 @@ if ( !defined('ABSPATH')) exit;
         <?php responsive_widgets(); // responsive above widgets hook ?>
 
 			<?php if (!dynamic_sidebar('home-widget-2')) : ?>
-            <div class="widget-wrapper">
-
-                <div class="widget-title-home"><h3><?php _e('Home Widget 2', 'responsive'); ?></h3></div>
-                <div class="textwidget"><?php _e('This is your second home widget box. To edit please go to Appearance > Widgets and choose 7th widget from the top in area 7 called Home Widget 2. Title is also manageable from widgets as well.','responsive'); ?></div>
-
+        <div class="widget-wrapper">
+          <div class="widget-title-home"><h3><?php _e('Home Widget 2', 'responsive'); ?></h3></div>
+          <div class="textwidget"><?php _e('This is your second home widget box. To edit please go to Appearance > Widgets and choose 7th widget from the top in area 7 called Home Widget 2. Title is also manageable from widgets as well.','responsive'); ?></div>
 			</div> <!-- end of .widget-wrapper -->
 			<?php endif; //end of home-widget-2 ?>
 
